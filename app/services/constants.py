f2779e9d1eadf9968bd73ccf11b5b12fb9681ba7
Anastasiya Borisycheva <borisycheva.a@yandex.ru>
2025-02-16 08:47:04 +0300

    second review

--- app/services/constants.py
+++ app/services/constants.py
@@ -1,12 +1,7 @@
-from datetime import datetime
-
 from app.core.config import settings
 
-FORMAT = "%Y-%m-%d %H:%M:%S"
 
-now_date_time = datetime.now().strftime(FORMAT)
 SPREADSHEET_BODY = {
-    "properties": {"title": f"Отчёт на {now_date_time}", "locale": "ru_RU"},
     "sheets": [
         {
             "properties": {
@@ -27,7 +22,6 @@ PERMISSIONS_BODY = {
 
 
 TABLE_VALUES = [
-    ["Отчёт от", now_date_time],
     ["Топ проектов по скорости закрытия"],
     ["Название проекта", "Время сбора", "Описание"],
 ]
--- app/services/google_api.py
+++ app/services/google_api.py
@@ -1,13 +1,25 @@
+from datetime import datetime
+
 from aiogoogle import Aiogoogle
 
 from .constants import (PERMISSIONS_BODY, SPREADSHEET_BODY, TABLE_VALUES,
                         UPDATE_BODY)
 
+FORMAT = "%Y-%m-%d %H:%M:%S"
+
 
 async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
     service = await wrapper_services.discover('sheets', 'v4')
+
+    now_date_time = datetime.now().strftime(FORMAT)
+    spreadsheet_body = SPREADSHEET_BODY
+    spreadsheet_body['properties'] = {
+        "title": f"Отчёт на {now_date_time}",
+        "locale": "ru_RU"
+    }
+
     response = await wrapper_services.as_service_account(
-        service.spreadsheets.create(json=SPREADSHEET_BODY)
+        service.spreadsheets.create(json=spreadsheet_body)
     )
     spreadsheet_id = response['spreadsheetId']
     return spreadsheet_id
@@ -33,7 +45,9 @@ async def spreadsheets_update_value(
         wrapper_services: Aiogoogle
 ) -> None:
     service = await wrapper_services.discover('sheets', 'v4')
+    now_date_time = datetime.now().strftime(FORMAT)
     table_values = TABLE_VALUES
+    table_values.insert(0, ["Отчёт от", now_date_time])
 
     for res in charity_projects:
         table_values.append(res)
